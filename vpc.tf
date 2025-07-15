e613492d690deced252adaaec2fade86b2e6ccca
karna <karunakark1729@gmail.com>
2025-07-15 16:59:38 +0530

    terraform-aws-vpc

--- vpc.tf
+++ vpc.tf
@@ -5,6 +5,7 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = "true"
 
   tags = merge(
+    var.vpc_tags,
     local.common_tags,
     {
       Name = "${var.project}-${var.environment}"
@@ -25,18 +26,47 @@ resource "aws_internet_gateway" "main" {
 }
 
 #roboshop-dev-us-east-1a
+resource "aws_subnet" "private" {
+  count      = length(var.public_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidrs[count.index]
+
+  availability_zone       = local.az_names[count.index]
+  # map_public_ip_on_launch = true
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-private-${local.az_names[count.index]}"
+    }
+  )
+}
+
 resource "aws_subnet" "public" {
   count      = length(var.public_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet_cidrs[count.index]
 
-  availability_zone       = slice(data.aws_availability_zones.available.names, 0, 2)[count.index]
-
-  map_public_ip_on_launch = true
+  availability_zone       = local.az_names[count.index]
+  # map_public_ip_on_launch = true
   tags = merge(
     local.common_tags,
     {
       Name = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
+    }
+  )
+}
+
+resource "aws_subnet" "database" {
+  count      = length(var.database_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.database_subnet_cidrs[count.index]
+
+  availability_zone       = local.az_names[count.index]
+  # map_public_ip_on_launch = true
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-database-${local.az_names[count.index]}"
     }
   )
 }
